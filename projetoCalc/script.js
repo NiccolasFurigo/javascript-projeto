e8d43733b5dff47755aed66529303b9c64ceda17
NiccolasFurigo <niccolas.furigo.souza@gmail.com>
2021-03-19 15:06:18 -0300

    Adicionando JS

---
+++ projetoCalc/script.js
@@ -0,0 +1,12 @@
+
+let res = document.querySelector('div#tela')
+
+
+function clicar(){
+    let num = Number(document.getElementById('number').value)
+    window.alert(`${num}`)
+}
+
+function calc() {
+    res.innerHTML = `${num.value}`
+}
