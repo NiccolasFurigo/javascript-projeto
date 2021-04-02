92f25a90eef7fd4a872a7508d77d2926cd111429
NiccolasFurigo <niccolas.furigo.souza@gmail.com>
2021-04-02 12:45:18 -0300

    CSS Complete

---
+++ SuperTrunfo/supertrunfo.js
@@ -0,0 +1,18 @@
+var carta1 = {
+    nome: "Endymion",
+    atributos: {
+        ataque: 30,
+        defesa: 50,
+        magia: 100
+    }
+}
+var carta02 = {
+    nome: "Mago Negro",
+    atributos: {
+        ataque: 20,
+        defesa: 60,
+        magia: 95
+    }
+}
+
+var cartas = [carta1, carta02]
